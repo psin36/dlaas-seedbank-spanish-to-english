78846e310b048790c86bbc305afd5b18b0554d4c
Prachi S <psin@mit.edu>
2018-08-10 11:46:05 -0400

    cleaning up repo

--- myscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-nvidia-smi --query-gpu=index,name,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 5  > $LOG_DIR/gpulogs.csv 2>&1 &
-pip uninstall -y tensorflow-tensorboard
-pip install --upgrade tensorflow-gpu
-pip install --upgrade keras
-export PYTHONIOENCODING=utf-8
-python3 nvidia-smi-nmt_with_attention.py --number_examples=10000
